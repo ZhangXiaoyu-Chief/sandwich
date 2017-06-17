692f89fa3136d565931b46910681afad439eb202
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-06-18 00:08:55 +0800

    前端框架加入了分页和搜索

--- sandwich/urls.py
+++ sandwich/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'cmdb/', include('cmdb.urls')),
+    url(r'api/', include('api.urls')),
 ]
--- static/app/cmdb/server-list/server-list.component.js
+++ static/app/cmdb/server-list/server-list.component.js
@@ -7,9 +7,9 @@ angular.
   module('serverList').
   component('serverList', {
     templateUrl: '/static/app/cmdb/server-list/server-list.template.html',
-    controller: ['$http','Toastr', function ServerlistListController($http, Toastr) {
+    controller: ['$http','Toastr', function ($http, Toastr) {
       var self = this;
-      self.labels ={
+      this.labels = {
           '在线':"label-success",
           '已下线':"label-default",
           '未知':"label-warning",
@@ -17,9 +17,55 @@ angular.
           '备用':"label-info",
           '报废':"label-default"
       };
-      $http.get('/cmdb/server_list/').then(function(response) {
-        self.servers = response.data;
-        // Toastr['success']("Gnome & Growl type non-blocking notifications", "Toastr Notifications");
-      });
+      this.num_page = 1;
+      this.page = 1;
+      this.per_page = 20;
+      this.search_input = "";
+      this.search = "";
+
+      this.get_pages = function () {
+          var pages = [];
+          for(var i=1;i<=this.num_page;i++){
+              pages.push(i);
+          }
+          return pages;
+      };
+      this.is_active = function (p) {
+          if(p === self.page){
+              return 'active';
+          }else {
+              return "";
+          }
+      };
+      this.get_data = function () {
+          $http.get('/api/server/list/?page=' + self.page + '&per_page=' + self.per_page + '&search=' + self.search_data).then(function(response) {
+            self.servers = response.data.data;
+            self.num_page = response.data.total_page;
+          }, function () {
+              // 获取数据失败执行
+          });
+      };
+      this.get_data();
+      this.change_page = function (page) {
+          if(this.page !== page){
+              this.page = page;
+              this.get_data();
+          }
+      };
+      this.search = function (e) {
+          self.search_data = self.search_input;
+          self.page = 1;
+          self.get_data();
+      };
+      this.previous_page = function (e) {
+          if(self.page >1){
+              self.page -= 1;
+          }
+      };
+      this.next_page = function () {
+          if(self.page < self.num_page){
+              self.page += 1;
+          }
+      };
     }]
   });
