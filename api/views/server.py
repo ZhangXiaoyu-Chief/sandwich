b18d221ce5943f6a38e82d2eafc5ae45d490a21e
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-06-20 16:52:03 +0800

    增加了主机模糊搜索

--- api/views/server.py
+++ api/views/server.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from api.libs.base import CoreView
 from cmdb.models import Asset
-
+from django.db.models import Q
 
 class Server(CoreView):
     def get_list(self):
-        server_objs = self.page_split(Asset.objects.filter(asset_type='server').all())
+        search = self.parameters('search')
+        if not search:
+            server_objs = self.page_split(Asset.objects.filter(asset_type='server').all())
+        else:
+            server_objs = self.page_split(Asset.objects.filter(asset_type='server').filter(Q(name__contains=search) | Q(nics__ip_address__contains=search) | Q(asset_num__contains=search)))
         server_list = []
         for server_obj in server_objs:
             server_list.append(server_obj.get_base_info())
