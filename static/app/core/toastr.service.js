db94f02cc2a9de7e330d8382df09bfcbce109e58
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-06-28 23:20:19 +0800

    解决了错误信息的BUG（报错）

--- static/app/core/toastr.service.js
+++ static/app/core/toastr.service.js
@@ -21,17 +21,34 @@ angular.
           "hideMethod": "fadeOut"
         };
         var message_handle = function (response, action) {
+            mytoastr.options = {
+                      "closeButton": true,
+                      "debug": false,
+                      "positionClass": "toast-bottom-right",
+                      "onclick": null,
+                      "showDuration": "1000",
+                      "hideDuration": "1000",
+                      "timeOut": "3000",
+                      "extendedTimeOut": "1000",
+                      "showEasing": "swing",
+                      "hideEasing": "linear",
+                      "showMethod": "fadeIn",
+                      "hideMethod": "fadeOut"
+                    };
             if (response.status === 401) {
                 window.location.href = response.data.data.login_url
             }
             if(response.status === 403){
-                self.mytoastr["error"]("对不起，您没有执行此操作的权限", "权限错误");
+                mytoastr["error"]("对不起，您没有执行此操作的权限", "权限错误");
             }
             if(response.status === 416){
-                self.mytoastr["error"](action + "失败，要操作的对象不存在或已经删除", "错误");
+                mytoastr["error"](action + "失败，违反某些字段的唯一性约束", "错误");
+            }
+            if(response.status === 404){
+                mytoastr["error"](action + "失败，要操作的对象不存在或已经删除", "错误");
             }
             if(response.status === 500){
-                self.mytoastr["error"](action + "失败", "未知错误");
+                mytoastr["error"](action + "失败", "未知错误");
             }
         };
         return {
