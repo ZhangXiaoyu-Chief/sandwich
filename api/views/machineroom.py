fd8dabd1890f9d25433789fae1d07636c56bfef7
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-07-01 10:12:12 +0800

    添加新增机房功能

--- api/views/machineroom.py
+++ api/views/machineroom.py
@@ -1,6 +1,7 @@
 from api.libs.base import CoreView
-from cmdb.models import MachineRoom
+from cmdb.models import MachineRoom, DataCenter
 from account.models import UserProfile
+from django.db.utils import IntegrityError
 
 
 class MachineRoomView(CoreView):
@@ -18,3 +19,23 @@ class MachineRoomView(CoreView):
         for machineroom_obj in machineroom_objs:
             machineroom_list.append(machineroom_obj.get_info())
         self.response_data["data"] = machineroom_list
+
+    def post_create(self):
+        try:
+            name = self.parameters("name")
+            contact = self.parameters("contact")
+            memo = self.parameters("memo")
+            address = self.parameters("address")
+            admin_id = int(self.parameters("admin"))
+            datacenter_id = int(self.parameters("datacenter"))
+            admin_obj = UserProfile.objects.filter(id=admin_id).first()
+            datacenter_obj = DataCenter.objects.filter(id=datacenter_id).first()
+            if admin_obj and admin_obj.user:
+                new_machineroom_obj = MachineRoom(name=name, contact=contact, memo=memo, admin=admin_obj.user, address=address, center=datacenter_obj)
+            else:
+                new_machineroom_obj = MachineRoom(name=name, contact=contact, memo=memo, address=address, center=datacenter_obj)
+            new_machineroom_obj.save()
+            self.response_data['data'] = new_machineroom_obj.get_info()
+        except IntegrityError:
+            self.response_data['status'] = False
+            self.status_code = 416
--- static/app/cmdb/machineroom-list/machineroom-list.component.js
+++ static/app/cmdb/machineroom-list/machineroom-list.component.js
@@ -20,6 +20,21 @@ angular.module('machineroomList').component('machineroomList', {
                 Toastr.handle(response,"获取机房列表");
                 self.loading = false;
             });
+            $http.get('/api/datacenter/list/?page=' + self.page + '&per_page=' + 0).then(function (response) {
+                self.datacenters_select = response.data.data;
+                self.loading = false;
+            }, function (response) {
+                // 获取数据失败执行
+                Toastr.handle(response,"获取数据中心");
+                self.loading = false;
+            });
+            $http.get('/api/account/list/').then(function (response) {
+                self.admins_select = response.data.data;
+                self.loading = false;
+            }, function (response) {
+                // 获取数据失败执行
+                self.loading = false;
+            });
 
         };
         this.get_data();
@@ -52,6 +67,54 @@ angular.module('machineroomList').component('machineroomList', {
             } else {
                 return "";
             }
+        };
+        this.init_create_form_data = function (form) {
+            // self.loading = true;
+            $.fn.modal.Constructor.prototype.enforceFocus = function() {};
+            self.create_form_data = {
+                name: "",
+                datacenter:0,
+                contact: "",
+                admin: 0,
+                memo: "",
+                address:""
+            };
+            $("#datacenter").val(0)
+            $("#datacenter").select2({
+                language: "zh-CN", //设置 提示语言
+                width: "100%", //设置下拉框的宽度
+            });
+            $("#admin").val(0)
+            $("#admin").select2({
+                language: "zh-CN", //设置 提示语言
+                width: "100%", //设置下拉框的宽度
+            });
+            form.name.$dirty = false;
+            form.name.$pristine = true;
+        };
+        this.create_datacenter = function (form) {
+            if (!form.$invalid) {
+                self.loading = true
+                var postCfg = {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    transformRequest: function (data) {
+                        return $.param(data);
+                    }
+                };
+                var request_data = self.create_form_data;
+                $http.post("/api/machineroom/create/", request_data, postCfg)
+                    .then(function (response) {
+                        self.loading = false;
+                        Toastr.messager["success"]("创建成功", "成功");
+                        self.get_data();
+                        $('#create-model').modal('hide');
+                    }, function (response) {
+                        // 获取数据失败执行
+                        Toastr.handle(response, "创建机房");
+                        self.loading = false;
+                    });
+            }
+
         };
     }]
 });
