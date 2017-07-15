4d3a36fbe743a8755a38b2453a951ba4c9caad6e
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-07-15 22:05:42 +0800

    删除了调试代码

--- api/views/server.py
+++ api/views/server.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from api.libs.base import CoreView
-from cmdb.models import Asset,BusinessUnit,Cabinet,Tags
+from cmdb.models import Asset, BusinessUnit, Cabinet, Tags
 from django.db.models import Q
 from api.libs.cmdb_agent import CmdbCollector
 from api.libs.asset_handler import AssetHandler
@@ -13,6 +13,7 @@ from account.models import UserProfile
 
 class Server(CoreView):
     login_required_action = ["get_list"]
+
     # permission_view_map = {
     #     "get_list": "can_view_asset"
     # }
@@ -26,8 +27,8 @@ class Server(CoreView):
         else:
             server_objs = self.page_split(Asset.objects.filter(
                 business_unit__in=get_objects_for_user(self.request.user, "cmdb.view_project_asset"))
-                .filter(Q(name__contains=search) | Q(nics__ip_address__contains=search) |
-                        Q(asset_num__contains=search)).distinct())
+                                          .filter(Q(name__contains=search) | Q(nics__ip_address__contains=search) |
+                                                  Q(asset_num__contains=search)).distinct())
         server_list = []
         for server_obj in server_objs:
             server_list.append(server_obj.get_base_info())
@@ -49,8 +50,10 @@ class Server(CoreView):
                 elif "Authentication failure" in cmdb_collector.msg:
                     response_data.append({"ipaddress": ipaddress, "status": False, "msg": "认证失败，请检查用户名密码是否正确"})
             else:
-                if not Asset.objects.filter(sn=cmdb_collector.asset_info.get('essential_information').get("SN")).first():
-                    handler = AssetHandler(self.request, cmdb_collector.asset_info, project_id=project_id, management_ip=ipaddress)
+                if not Asset.objects.filter(
+                        sn=cmdb_collector.asset_info.get('essential_information').get("SN")).first():
+                    handler = AssetHandler(self.request, cmdb_collector.asset_info, project_id=project_id,
+                                           management_ip=ipaddress)
                     handler.create_asset('server')
                     response_data.append({"ipaddress": ipaddress, "status": True, "msg": "添加成功"})
                 else:
@@ -84,33 +87,27 @@ class Server(CoreView):
             self.status_code = 500
 
     def post_change(self):
-        print(self.request.POST)
         asset_id = self.parameters("id")
         filed_name = self.parameters("filed_name")
         value = self.parameters("new_value")
-        if value=="":
+        if value == "":
             value = None
         asset_obj = Asset.objects.filter(id=asset_id).first()
         if asset_obj:
-            print(1)
             filed_name = filed_name.split(".")
             if filed_name[0] == 'base':
                 if filed_name[1] not in ["admin", "raid_type", "cabinet", "business_unit", "operation", "tags"]:
-                    print(134)
                     setattr(asset_obj, filed_name[1], value)
                     asset_obj.save()
                 if filed_name[1] == "cabinet":
-                    print(22121)
                     cabinet_obj = Cabinet.objects.filter(id=value).first()
                     asset_obj.cabinet = cabinet_obj
                     asset_obj.save()
                 if filed_name[1] == "business_unit":
-                    print(22121)
                     business_unit_obj = BusinessUnit.objects.filter(id=value).first()
                     asset_obj.business_unit = business_unit_obj
                     asset_obj.save()
                 if filed_name[1] == "admin":
-                    print(22121)
                     admin_obj = UserProfile.objects.filter(id=value).first()
                     if admin_obj:
                         asset_obj.admin = admin_obj.user
@@ -119,7 +116,6 @@ class Server(CoreView):
                         asset_obj.admin = None
                         asset_obj.save()
                 if filed_name[1] == "operation":
-                    print(22121)
                     operation_obj = UserProfile.objects.filter(id=value).first()
                     if operation_obj:
                         asset_obj.operation = operation_obj.user
@@ -127,9 +123,8 @@ class Server(CoreView):
                     else:
                         asset_obj.operation = None
                         asset_obj.save()
-
                 if filed_name[1] == "tags":
-                    asset_obj.tags= []
+                    asset_obj.tags = []
                     if value:
                         tags_list = value.split(",")
                         for tag in tags_list:
@@ -142,15 +137,11 @@ class Server(CoreView):
                                 asset_obj.tags.add(tag_obj)
                     asset_obj.save()
 
-
             if filed_name[0] == 'server':
                 setattr(asset_obj.server, filed_name[1], value)
                 asset_obj.server.save()
 
-            # print()
-            # print(asset_obj.get_info())
 
         else:
             self.response_data['status'] = False
             self.status_code = 404
-
